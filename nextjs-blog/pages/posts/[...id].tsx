d82b2396fbd5a843f70b119e3082baa9b9724737
euije <exitfill1234@naver.com>
2022-11-06 03:16:26 +0900

    (fix : implement essential props)

--- nextjs-blog/pages/posts/[...id].tsx
+++ nextjs-blog/pages/posts/[...id].tsx
@@ -1,10 +1,23 @@
+export default function PostDynamic({ data }) {
+    return (<></>);
+}
+
 export async function getStaticPaths() {
     return {
-        id: ['a', 'b', 'c'],
+        paths: [{
+            params: {
+                id: ['a', 'b', 'c'],
+            },
+        }],
         fallback: false,
     };
 }
 
 export async function getStaticProps({ params }) {
     // params.id will be like ['a', 'b', 'c']
+    return {
+        props: {
+            data: "dummy"
+        }
+    };
 }
