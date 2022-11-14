3510b27f74008b7b688cc14862211d070f09b392
euije <exitfill1234@naver.com>
2022-11-14 10:21:35 +0900

    PR test

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,14 @@
 // This Custom 404 page file is statically generated at build time.
 
 export default function Custom404() {
-    return (<h1>404 - Page Not Found</h1>);
+    return (
+        <>
+            <h1>
+                404 - Page Not Found
+            </h1>
+            <div>
+                PR test
+            </div>
+        </>
+    );
 }
